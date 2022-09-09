6b8ebec931f26df9c2b87101379872424c575139
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-09 19:17:57 +0530

    Welcome to EmpWage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empbuilder Dmart = new Empbuilder("Dmart", 20, 2, 10);
+            Empbuilder Reliance = new Empbuilder("Reliance", 10, 4, 20);
+            Dmart.ComputeWage();
+            Console.WriteLine(Dmart.ToString());
+            Reliance.ComputeWage();
+            Console.WriteLine(Reliance.ToString());
+        }
+    }
+    
+}
